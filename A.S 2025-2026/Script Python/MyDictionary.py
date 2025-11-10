460de81ec8cfc2b6e9889230879ddb87c80e542a
Lorydima <lorenzo.dimarzo.ld@gmail.com>
2025-11-10 13:01:15 +0100

    Add MyDictionary.py with user information

---
+++ A.S 2025-2026/Script Python/MyDictionary.py
@@ -0,0 +1,15 @@
+# Lezione 10/11/2025 MyDictionary
+
+user = {
+    "Name": "Lorenzo",
+    "Surname" : "Di Marzo",
+    "Age" : "16",
+    "Born City" : "Naples",
+    "School" : "G.B Vico",
+    "Favourite Color" : "Red",
+    "Favourite Food": "Pizza",
+}
+
+print(len(user))
+print(user.items())
+
