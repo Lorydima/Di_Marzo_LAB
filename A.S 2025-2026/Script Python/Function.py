a3d5ceedcce7f7eca42bad6e12bdb60b353c532b
Lorydima <lorenzo.dimarzo.ld@gmail.com>
2025-11-17 13:05:38 +0100

    Create Function.py

---
+++ A.S 2025-2026/Script Python/Function.py
@@ -0,0 +1,8 @@
+# Lezione 17/11/2025 Function
+
+def sum():
+    number_1 = input("Insert number 1:")
+    number_2 = input("Insert number 2:")
+    return float(number_1) + float(number_2)
+
+print("Result =", sum())
