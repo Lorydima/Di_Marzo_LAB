ca379a3b11ae5257c4a7cc9965f38d310cc5ac4f
Lorydima <lorenzo.dimarzo.ld@gmail.com>
2026-02-02 13:04:31 +0100

    Update Turtle_03.py

--- A.S 2025-2026/Script Python/Turtle_03.py
+++ A.S 2025-2026/Script Python/Turtle_03.py
@@ -1,4 +1,4 @@
-# Lezione 26/01/2026 "Turtle_03"
+# Lezione 26/01/2026 e 02/02/2026 "Turtle_03"
 
 # Library for program Dev
 import turtle
@@ -9,6 +9,7 @@ screen = turtle.Screen()
 screen.title("Beating Heart")
 screen.setup(1000,1000)
 screen.setworldcoordinates(-1000,-1000,1000,1000)
+screen.bgcolor("black")
 
 # Turtle Setup
 turtle.speed(0)
@@ -16,7 +17,14 @@ turtle.hideturtle()
 screen.tracer(0,0)
 turtle.color("red")
 
+# Animation Coordinates
+a = 220
+sign = -1
+
 # Heart Function
+'''
+This function draw the heart
+'''
 def heart(alpha,d):
     radius = d/math.tan(math.radians(180-alpha/2))
     turtle.up()
@@ -30,3 +38,24 @@ def heart(alpha,d):
     turtle.circle(radius,alpha)
     turtle.forward(d)
     turtle.end_fill()
+
+# Animate Function
+'''
+This function generate the animations
+'''
+def animate():
+    global a , sign
+    turtle.clear()
+    heart(a,1000)
+    screen.update()
+    a += sign
+    if a < 215:
+        sign = -sign
+    elif a > 220:
+        sign = -sign
+    screen.ontimer(animate,50)
+
+
+# Call Main Function
+animate()
+turtle.done()
